a12727e183df982d092cc074b618ec0b74571d37
Ted Wahrburg <ted.wahrburg@gmail.com>
2012-01-02 22:06:07 -0500

    First Commit

---
+++ LegoRadarClock.ino
@@ -0,0 +1,20 @@
+/*
+Arduino Based Lego Train "Radar Clock"
+(C) Theodore "Waterbury" Wahrburg; 2012
+
+V.0.0.1
+
+*/
+
+void setup() {
+  //Sets up Serial Port; Inform Console
+  Serial.begin(9600);
+  Serial.println("Arduino Based Lego Train \"Radar Clock\"");
+  Serial.println("(C) Theodore \"Waterbury\" Wahrburg; 2012");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
