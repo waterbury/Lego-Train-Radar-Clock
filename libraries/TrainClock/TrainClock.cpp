7cf3a325f3300d2f5c74d614124422ba980910bd
unknown <Ted@Laptop-VAIO.(none)>
2012-04-24 00:21:12 -0400

    Adding CounterClockwise Code

--- libraries/TrainClock/TrainClock.cpp
+++ libraries/TrainClock/TrainClock.cpp
@@ -6,31 +6,31 @@
 
 TrainClock::TrainClock ()
 {
-_timeSinceLast = 1000000;
-setLastTime();
-_millisAtLastCall = millis();
+	_timeSinceLast = 1000000;
+	setLastTime();
+	_millisAtLastCall = millis();
 
-blipArray[0][0] = 1;
+	blipArray[0][0] = 1;
 
 
-setTimeMillis( millis() );
+	setTimeMillis( millis() );
 
 }
 
 
 
 void TrainClock::setLastTime(){
-_lastTime = micros(); 
+	_lastTime = micros(); 
 }
 
 
 
 //Finds time since stop watch began. Should be able to measure up about 71 minutes in length, else results will overflow back to zero
 unsigned long TrainClock::getTimeSinceLast()
-	{
+{
 	
-//Micros is a 32-bit value,  Arduino Atmega8 series chips overflow Micros() after a little over 71 minutes.
-//If overflowed to zero, add 32-bit max to current micros and subtract previous value
+	//Micros is a 32-bit value,  Arduino Atmega8 series chips overflow Micros() after a little over 71 minutes.
+	//If overflowed to zero, add 32-bit max to current micros and subtract previous value
 	if (micros() < _lastTime)
 	{
 		_timeSinceLast = 4294967295L + 1 + micros() - _lastTime;
@@ -38,44 +38,44 @@ unsigned long TrainClock::getTimeSinceLast()
 	else
 	{
 		_timeSinceLast = micros() - _lastTime;;
-	
-	 
-	 }
-	 
-	return _timeSinceLast;
+		
+		
 	}
 	
+	return _timeSinceLast;
+}
 
-	
-	
+
+
+
 long TrainClock::getTimeMillis()
 {
 
-unsigned long long tmpMillis = millis();
+	unsigned long long tmpMillis = millis();
 
 
-if ( _millisAtLastCall > tmpMillis ) //+ _timeAtZero )
+	if ( _millisAtLastCall > tmpMillis ) //+ _timeAtZero )
 	setTimeMillis(    ( (tmpMillis + _timeAtZero + 4294967295LL + 1) % 86400000) );
 
-_millisAtLastCall = millis();
-long tmpOffsetTime = (tmpMillis + _timeAtZero) % 86400000;
+	_millisAtLastCall = millis();
+	long tmpOffsetTime = (tmpMillis + _timeAtZero) % 86400000;
 
-return tmpOffsetTime;
+	return tmpOffsetTime;
 
 }
 
 unsigned long TrainClock::setTimeMillis(long timeInMillis)
 {
 
-timeInMillis = timeInMillis % 86400000;
+	timeInMillis = timeInMillis % 86400000;
 
-_timeAtZero = timeInMillis - (millis()  % 86400000L);
+	_timeAtZero = timeInMillis - (millis()  % 86400000L);
 
-if (_timeAtZero < 0 )
- _timeAtZero += 86400000;
+	if (_timeAtZero < 0 )
+	_timeAtZero += 86400000;
 
-return _timeAtZero; 
- 
+	return _timeAtZero; 
+
 }
 
 void findBlipsCounterClockwise(unsigned long currentTimeMillis,double speedOfTrain)
@@ -129,65 +129,65 @@ int intersectionMinutes = int( speedOfTrain * ( (speedOfTrain + SPEED_OF_MINS) /
 int intersectionHours   = int( speedOfTrain * ( (speedOfTrain + SPEED_OF_HOURS) / hoursDeg) );
 
 
-      //Perform Modulus operation of the Intersect by 360. Ex. If value is 366, modulus would be 6. Draw 1 second hand in first loop.
-      intersectionSeconds = intersectionSeconds % 360;
-      intersectionMinutes = intersectionMinutes % 360;
-      intersectionHours = intersectionHours % 360;
+	//Perform Modulus operation of the Intersect by 360. Ex. If value is 366, modulus would be 6. Draw 1 second hand in first loop.
+	intersectionSeconds = intersectionSeconds % 360;
+	intersectionMinutes = intersectionMinutes % 360;
+	intersectionHours = intersectionHours % 360;
 
 */}
 
 
 void TrainClock::findBlipsClockwise(unsigned long currentTimeMillis, double speedOfTrain)
 {
-//int i = 0;
-//int j = 0;
+	//int i = 0;
+	//int j = 0;
 
 
-//Make sure current time is no more than 86400 seconds, then divide millis by 1000 to get actual seconds
-currentTimeMillis = currentTimeMillis % 43200000;
-float currentTime = currentTimeMillis / 1000.0;
+	//Make sure current time is no more than 43300 seconds for 12 hour time, then divide millis by 1000 to get actual seconds
+	currentTimeMillis = currentTimeMillis % 43200000;
+	float currentTime = currentTimeMillis / 1000.0;
 
-Serial.print("currentTime: ");
-Serial.println(currentTime);
+	Serial.print("currentTime: ");
+	Serial.println(currentTime);
 
 
 
-// find hours by dividing by seconds in hour. In order to make 12 hour time, must mod by 12..
-double hoursDeg = currentTime / 3600.0;
- 
-// if (hoursDeg >= 12)
-  //hoursDeg -= 12;
-  
-Serial.print("hoursDeg: ");
-Serial.println(hoursDeg);
+	// find hours by dividing by seconds in hour. In order to make 12 hour time, must mod by 12..
+	double hoursDeg = currentTime / 3600.0;
 
-// truncate hours to get decimal seconds, then multiply by 60 to get minutes
-double minutesDeg = (hoursDeg - int(hoursDeg)) * 60; 
+	// if (hoursDeg >= 12)
+	//hoursDeg -= 12;
 
-Serial.print("minutesDeg: ");
-Serial.println(minutesDeg);
+	Serial.print("hoursDeg: ");
+	Serial.println(hoursDeg);
 
-//truncate minutes to get decimal seconds, then multiply by 60 to get whole number. Truncate that value to get rational number
-int secondsDeg = int( (minutesDeg - int(minutesDeg) ) * 60);
+	// truncate hours to get decimal seconds, then multiply by 60 to get minutes
+	double minutesDeg = (hoursDeg - int(hoursDeg)) * 60; 
 
-Serial.print("secondsDeg: ");
-Serial.println(secondsDeg);
+	Serial.print("minutesDeg: ");
+	Serial.println(minutesDeg);
 
-//ex.
-//83678 secs = 
-//11.243888888888888888888888888889 hours
-//14.63333333333333333333333333334 minutes
-//(38).0000000000000000000000000004 seconds
+	//truncate minutes to get decimal seconds, then multiply by 60 to get whole number. Truncate that value to get rational number
+	int secondsDeg = int( (minutesDeg - int(minutesDeg) ) * 60);
 
-//hoursDeg = 0;
-//minutesDeg = 0;
-//secondsDeg = 30;
+	Serial.print("secondsDeg: ");
+	Serial.println(secondsDeg);
 
-hoursDeg *= 30; // multiply 12 hour hours by 30 to get degree around clock
-minutesDeg *= 6; // multiply minutes by 6 to get degree around clock
-secondsDeg *= 6; // multiply seconds by 6 to get degree around clock
+	//ex.
+	//83678 secs = 
+	//11.243888888888888888888888888889 hours
+	//14.63333333333333333333333333334 minutes
+	//(38).0000000000000000000000000004 seconds
 
-/*
+	//hoursDeg = 0;
+	//minutesDeg = 0;
+	//secondsDeg = 30;
+
+	hoursDeg *= 30; // multiply 12 hour hours by 30 to get degree around clock
+	minutesDeg *= 6; // multiply minutes by 6 to get degree around clock
+	secondsDeg *= 6; // multiply seconds by 6 to get degree around clock
+
+	/*
 hoursDeg -= OFFSET;
 if( hoursDeg <0)
 hoursDeg += 360;
@@ -203,11 +203,11 @@ secondsDeg += 360;
 */
 
 
-//speedOfTrain is degrees per second train is moving
+	//speedOfTrain is degrees per second train is moving
 
-//SPEED_OF_SECS is speed secondhand is moving per sec, ditto for SPEED_OF_MINS, SpeedOfHours
+	//SPEED_OF_SECS is speed secondhand is moving per sec, ditto for SPEED_OF_MINS, SpeedOfHours
 
-/*
+	/*
 6 degrees per sec
 	SPEED_OF_SECS = 6;
 6 degrees per 60 secs (1 minute) (6/60 = .01 degree per sec)
@@ -216,98 +216,98 @@ hours move 30 degrees per 3600 secs (1 hour) (30/3600 = 0.00833333.. degree per 
 	double SPEED_OF_HOURS = 0.0083333;
 */
 
-// (D)istance = (R)ate * (T)ime
-// Distance(Time hand was at 0) = ( speedOfTrain - speedOf[hand]) * Time ; when solving for Time..
-// Time = (speedOfTrain - speedOf[hand]) / (distanc of hand when @ 0)
-// Train will intersect in time found. 
-// If train is moving 180 degrees a sec and time is .5 sec, hand will intersect @ 90 degrees
-// intersection[Hand] = speedOfTrain * Time
+	// (D)istance = (R)ate * (T)ime
+	// Distance(Time hand was at 0) = ( speedOfTrain - speedOf[hand]) * Time ; when solving for Time..
+	// Time = (speedOfTrain - speedOf[hand]) / (distanc of hand when @ 0)
+	// Train will intersect in time found. 
+	// If train is moving 180 degrees a sec and time is .5 sec, hand will intersect @ 90 degrees
+	// intersection[Hand] = speedOfTrain * Time
 
 
-//If SpeedOfTrain is 180 deg/sec and sec hand is @ 180 deg mark (180 *(180 / (180 - 6) ) )
-//																(180* (180 / (174) ) )
-//
-
-
-
-// intersection[0] = intersectionSeconds; Turn on @ this degree
-// intersection[1] = intersectionMinutes; Turn on @ this degree 
-// intersection[2] = intersectionHours; Turn on @ this degree
-//	(180* ( 1.034482~) ) = 186.20689~
-
-double blah = ( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
-Serial.println(blah);
-
-blipArray[0][0] = int(blah); //speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
-blipArray[0][1] = 0;
-blipArray[0][2] = 1;
-
-blipArray[1][0] = int( speedOfTrain * ( minutesDeg / (speedOfTrain - SPEED_OF_MINS) ) );
-blipArray[1][1] = 1;
-blipArray[1][2] = 1;
-
-
-blipArray[2][0] = int( speedOfTrain * ( hoursDeg / (speedOfTrain - SPEED_OF_HOURS) ) );
-blipArray[2][1] = 2;
-blipArray[2][2] = 1;
-
-
-secondsDeg += DRAWLENGTH;
-if (secondsDeg >= 360)
- secondsDeg -= 360;
-
-minutesDeg += DRAWLENGTH;
-if (minutesDeg >= 360)
- minutesDeg -= 360;
-
-hoursDeg += DRAWLENGTH;
-if (hoursDeg >= 360)
- hoursDeg -= 360;
-
-// intersection[3] = intersectionSeconds; Turn off @ this degree
-// intersection[4] = intersectionMinutes; Turn off @ this degree 
-// intersection[5] = intersectionHours; Turn off @ this degree
+	//If SpeedOfTrain is 180 deg/sec and sec hand is @ 180 deg mark (180 *(180 / (180 - 6) ) )
+	//																(180* (180 / (174) ) )
+	//
 
 
 
-blah = ( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
-Serial.println(blah);
+	// intersection[0] = intersectionSeconds; Turn on @ this degree
+	// intersection[1] = intersectionMinutes; Turn on @ this degree 
+	// intersection[2] = intersectionHours; Turn on @ this degree
+	//	(180* ( 1.034482~) ) = 186.20689~
 
-blipArray[3][0] = int( blah);// speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS))   ); //int( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
-blipArray[3][1] = 0;//0 = sec
-blipArray[3][2] = 0;//off
+	double blah = ( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
+	Serial.println(blah);
+
+	blipArray[0][0] = int(blah); //speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
+	blipArray[0][1] = 0;
+	blipArray[0][2] = 1;
+
+	blipArray[1][0] = int( speedOfTrain * ( minutesDeg / (speedOfTrain - SPEED_OF_MINS) ) );
+	blipArray[1][1] = 1;
+	blipArray[1][2] = 1;
 
 
-blipArray[4][0] = int( speedOfTrain * ( minutesDeg / (speedOfTrain - SPEED_OF_MINS) ) );
-blipArray[4][1] = 1;//1 = min
-blipArray[4][2] = 0;//off
-
-blipArray[5][0] = int( speedOfTrain * ( hoursDeg / (speedOfTrain - SPEED_OF_HOURS) ) );
-blipArray[5][1] = 2;//2 = hours
-blipArray[5][2] = 0;//off
+	blipArray[2][0] = int( speedOfTrain * ( hoursDeg / (speedOfTrain - SPEED_OF_HOURS) ) );
+	blipArray[2][1] = 2;
+	blipArray[2][2] = 1;
 
 
-bubbleSortArray();
+	secondsDeg += DRAWLENGTH;
+	if (secondsDeg >= 360)
+	secondsDeg -= 360;
+
+	minutesDeg += DRAWLENGTH;
+	if (minutesDeg >= 360)
+	minutesDeg -= 360;
+
+	hoursDeg += DRAWLENGTH;
+	if (hoursDeg >= 360)
+	hoursDeg -= 360;
+
+	// intersection[3] = intersectionSeconds; Turn off @ this degree
+	// intersection[4] = intersectionMinutes; Turn off @ this degree 
+	// intersection[5] = intersectionHours; Turn off @ this degree
+
+
+
+	blah = ( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
+	Serial.println(blah);
+
+	blipArray[3][0] = int( blah);// speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS))   ); //int( speedOfTrain * ( secondsDeg / (speedOfTrain - SPEED_OF_SECS) ) );
+	blipArray[3][1] = 0;//0 = sec
+	blipArray[3][2] = 0;//off
+
+
+	blipArray[4][0] = int( speedOfTrain * ( minutesDeg / (speedOfTrain - SPEED_OF_MINS) ) );
+	blipArray[4][1] = 1;//1 = min
+	blipArray[4][2] = 0;//off
+
+	blipArray[5][0] = int( speedOfTrain * ( hoursDeg / (speedOfTrain - SPEED_OF_HOURS) ) );
+	blipArray[5][1] = 2;//2 = hours
+	blipArray[5][2] = 0;//off
+
+
+	bubbleSortArray();
 
 
 }
 
 void TrainClock::bubbleSortArray (void)
 {
-int tempDegrees = 0;
-int tempHand = 0;
-int tempSwitch = 0;
-int x;
+	int tempDegrees = 0;
+	int tempHand = 0;
+	int tempSwitch = 0;
+	int x;
 
 	for( x=0; x<6; x++)
 	{
-//	
-	for(int y=0; y<5; y++)
+		//	
+		for(int y=0; y<5; y++)
 		{
-		    //if degrees is larger than next..
+			//if degrees is larger than next..
 			if(blipArray[y][0]>blipArray[y+1][0])
 			{
-			    //log next degree and hand value
+				//log next degree and hand value
 				tempDegrees = blipArray[y+1][0];
 				tempHand = blipArray[y+1][1];
 				tempSwitch = blipArray[y+1][2];
@@ -324,13 +324,13 @@ int x;
 	}
 
 	/*for(x=0;x<6;x++)
-	 {
-	 Serial.print(blipArray[x][0]);
-	 Serial.print(" - ");
-	 Serial.print(blipArray[x][1]);
-	 Serial.print(" - ");
-	 Serial.println(blipArray[x][2]);
-	 
+	{
+	Serial.print(blipArray[x][0]);
+	Serial.print(" - ");
+	Serial.print(blipArray[x][1]);
+	Serial.print(" - ");
+	Serial.println(blipArray[x][2]);
+	
 	}*/
 }
 
@@ -338,12 +338,12 @@ int TrainClock::getBlipArray(int i, int j)
 {
 
 
-if(i<6 && i>=0)
+	if(i<6 && i>=0)
 	if (j<6 && j>=0)
-		return blipArray[i][j];
-		
-		
-return 0;
+	return blipArray[i][j];
+	
+	
+	return 0;
 
 }
 
--- puppies/puppies.ino
+++ puppies/puppies.ino
@@ -31,9 +31,9 @@ volatile int IR_Triggered = 0;
 
 double speedOfTrain = 0;  
 
- float speedDifference = 0;
- 
- 
+float speedDifference = 0;
+
+
 int count = 0;
 
 
@@ -42,7 +42,7 @@ int count = 0;
 //Function for Infrared LED Interrupt
 void IR_Trigger()
 {
-IR_Triggered = 1;
+	IR_Triggered = 1;
 
 }
 
@@ -58,155 +58,184 @@ TrainClock train;
 
 
 void setup() {
-  
-  
-  Serial.begin(9600);
+
+
+	Serial.begin(9600);
 
 
 
-train.setTimeMillis(63200000);
-train.setLastTime();
+	train.setTimeMillis(63200000);
+	train.setLastTime();
 
-  //Sets up LED pins to output
-  pinMode(LED_STATUS, OUTPUT);
-  pinMode(LED_SECONDS, OUTPUT);
-  pinMode(LED_MINUTES, OUTPUT);
-  pinMode(LED_HOURS, OUTPUT);
-  //Sets Up IR Pin as input, for interrupt
-  pinMode(IR_PIN, INPUT);
-  
-  pinMode(CLOCKWISE_DETECT, INPUT); // set pin to detect whether train is moving clockwise to input
-  digitalWrite(CLOCKWISE_DETECT, HIGH); // turn on pullup resistor for PIN. If High, train is moving clockwise
+	//Sets up LED pins to output
+	pinMode(LED_STATUS, OUTPUT);
+	pinMode(LED_SECONDS, OUTPUT);
+	pinMode(LED_MINUTES, OUTPUT);
+	pinMode(LED_HOURS, OUTPUT);
+	//Sets Up IR Pin as input, for interrupt
+	pinMode(IR_PIN, INPUT);
 
-  //Sets up IR PIN to Interrupt Microcontroller as IR Detector pulls pin low
-  attachInterrupt(0, IR_Trigger, FALLING);
-  
-  
-        digitalWrite(LED_STATUS, HIGH);
-  
-  //Initialize Timer1
-//  Timer1.initialize(500000);
-  //Timer1.attachInterrupt(drawTimer);
-/*  
-  Timer1.disablePwm(LED_HOURS);
-  Timer1.disablePwm(LED_MINUTES);
-  Timer1.disablePwm(LED_SECONDS);
-  Timer1.disablePwm(LED_STATUS);
+	pinMode(CLOCKWISE_DETECT, INPUT); // set pin to detect whether train is moving clockwise to input
+	digitalWrite(CLOCKWISE_DETECT, HIGH); // turn on pullup resistor for PIN. If High, train is moving clockwise
+
+	//Sets up IR PIN to Interrupt Microcontroller as IR Detector pulls pin low
+	attachInterrupt(0, IR_Trigger, FALLING);
+
+
+	digitalWrite(LED_STATUS, HIGH);
+
+	//Initialize Timer1
+	//  Timer1.initialize(500000);
+	//Timer1.attachInterrupt(drawTimer);
+	/*  
+Timer1.disablePwm(LED_HOURS);
+Timer1.disablePwm(LED_MINUTES);
+Timer1.disablePwm(LED_SECONDS);
+Timer1.disablePwm(LED_STATUS);
 */
 }
 
 void loop()
-  {
-    
-    if(IR_Triggered)
-    {
-      //Detaches IR Interrupt
-      //detachInterrupt(0);
-      IR_Triggered = 0;
-      digitalWrite(LED_STATUS, LOW);
-      
-      if( train.getTimeSinceLast() > 1500000)
-      {
-        
-    //    printTime();
-        
-        
-        //Speed of train is determined by dividing the 360 degrees of the track circle by the time taken.
-        speedOfTrain = 90;//360/ (train.getTimeSinceLast()/1000000.0);
-    //    Serial.print("speedOfTrain: ");
-      //  Serial.println(speedOfTrain);
-        
-        train.setLastTime();
-        digitalWrite(LED_STATUS, HIGH);   
-        //If train is moving clockwise, CLOCKWISE_DETECT will be HIGH, else if counter-clockwise pin will be LOW
-        if ( digitalRead(CLOCKWISE_DETECT) == HIGH)
-        {
-       //   digitalWrite(LED_HOURS,1);
-          train.findBlipsClockwise( train.getTimeMillis(), speedOfTrain );
-        //  digitalWrite(LED_MINUTES,1);
-        }
-        else
-        {
-        }
-      
+{
+	
+	if(IR_Triggered)
+	{
+		//Detaches IR Interrupt
+		//detachInterrupt(0);
+		IR_Triggered = 0;
+		digitalWrite(LED_STATUS, LOW);
+		
+		if( train.getTimeSinceLast() > 1500000)
+		{
+			
+			//    printTime();
+			
+			//Speed of train is determined by dividing the 360 degrees of the track circle by the time taken.
+			speedOfTrain = 360/ (train.getTimeSinceLast()/1000000.0);
+			
+			train.setLastTime();
+			digitalWrite(LED_STATUS, HIGH);   
+			
+			//If train is moving clockwise, CLOCKWISE_DETECT will be HIGH, else if counter-clockwise pin will be LOW
+			if ( digitalRead(CLOCKWISE_DETECT) == HIGH)
+			{
+				train.findBlipsClockwise( train.getTimeMillis(), speedOfTrain );
+			}
+			else
+			{
+				train.findBlipsCounterClockwise( train.getTimeMillis(), speedOfTrain );
+			}
+			
 
-      count = 0;
+			count = 0;
 
-      }
-     }
-     
-     if (count<6)
-       if ( int(speedOfTrain / 1000000.0 * (train.getTimeSinceLast() ) )  >= (train.getBlipArray(count,0) )  )
-        {
-          Serial.print("count: ");
-          Serial.println (count);
-          Serial.println ( int(speedOfTrain /1000000.0 * (train.getTimeSinceLast() ) ) );
-          
-          
-          if ( train.getBlipArray(count,0) <=360)
-          {
-            Serial.print("Blip : ");
-            Serial.print(train.getBlipArray(count,0));
-            Serial.print(" Type: ");
-             Serial.print(train.getBlipArray(count,1));
-            Serial.print(" switch: ");           
-             Serial.println(train.getBlipArray(count,2));
-             Serial.println("-----------------");
+		}
+	}
+	
+	
+	if ( digitalRead(CLOCKWISE_DETECT) == HIGH)
+	{
+		if (count<6)
+		if ( int(speedOfTrain / 1000000.0 * (train.getTimeSinceLast() ) )  >= (train.getBlipArray(count,0) )  )
+		{
+			
+			/*Serial.print("count: ");
+			Serial.println (count);
+			Serial.println ( int(speedOfTrain /1000000.0 * (train.getTimeSinceLast() ) ) );*/
+			
+			
+			if ( train.getBlipArray(count,0) <=360)
+			{
+				/*Serial.print("Blip : ");
+			Serial.print(train.getBlipArray(count,0));
+			Serial.print(" Type: ");
+			Serial.print(train.getBlipArray(count,1));
+			Serial.print(" switch: ");           
+			Serial.println(train.getBlipArray(count,2));
+			Serial.println("-----------------");*/
+				
+				drawHand(train.getBlipArray(count,1), train.getBlipArray(count,2) );
+			}
+			
+			count++; 
+		}
+	}
+	else
+	{
+		if (count >= 0)
+		if ( 360 - int(speedOfTrain/1000000.0*(train.getTimeSinceLast())) )   <= (train.getBlipArray(count,0) )  )
+		{
+			
+			if ( train.getBlipArray(count,0) >=0)
+			{
+				drawHand(train.getBlipArray(count,1), train.getBlipArray(count,2) );
+			}			
+			
+			count--;
+			
+		}
+		
+		
+		
+	}
+	
+	//   Serial.print(train.getTimeSinceLast() );   
+	// Serial.print(": ");   
+	//Serial.println(  train.getBlipArray(0,0));
+	
+}
 
-            
-          if (train.getBlipArray(count,1) == 0)
-          digitalWrite(LED_SECONDS, train.getBlipArray(count,2) ) ;
-          
-          else if (train.getBlipArray(count,1) == 1)
-          digitalWrite(LED_MINUTES, train.getBlipArray(count,2) ) ;
 
-          else if (train.getBlipArray(count,1) == 2)
-          digitalWrite(LED_HOURS, train.getBlipArray(count,2) ) ;
-          }
-          
-         count++; 
-        }  
-  //   Serial.print(train.getTimeSinceLast() );   
-    // Serial.print(": ");   
-     //Serial.println(  train.getBlipArray(0,0));
-    
-  }
+void drawHand(int hand, int value)
+{
+	
+	if (hand == 0)
+	digitalWrite(LED_SECONDS, value ) ;
+	
+	else if (hand == 1)
+	digitalWrite(LED_MINUTES, value ) ;
+
+	else if (hand == 2)
+	digitalWrite(LED_HOURS, value ) ;
+
+
+}
 
 
 /*
 
 void printTime(void){
-  
- long day = 86400000; // 86400000 milliseconds in a day
- long hour = 3600000; // 3600000 milliseconds in an hour
- long minute = 60000; // 60000 milliseconds in a minute
- long second =  1000; // 1000 milliseconds in a second
 
-  
-  
- long timeNow = train.getTimeMillis();
+long day = 86400000; // 86400000 milliseconds in a day
+long hour = 3600000; // 3600000 milliseconds in an hour
+long minute = 60000; // 60000 milliseconds in a minute
+long second =  1000; // 1000 milliseconds in a second
 
- int days = timeNow / day ;                                //number of days
- int hours = (timeNow % day) / hour;                       //the remainder from days division (in milliseconds) divided by hours, this gives the full hours
- int minutes = ((timeNow % day) % hour) / minute ;         //and so on...
- int seconds = (((timeNow % day) % hour) % minute) / second;
-  
-  // put your main code here, to run repeatedly: 
- Serial.print("          Time: H:");
- Serial.print(hours);
- Serial.print(" M:");
- Serial.print(minutes);
- Serial.print(" S:");
- Serial.print(seconds);
- Serial.print(" Millis:");
 
- Serial.println(millis());
+
+long timeNow = train.getTimeMillis();
+
+int days = timeNow / day ;                                //number of days
+int hours = (timeNow % day) / hour;                       //the remainder from days division (in milliseconds) divided by hours, this gives the full hours
+int minutes = ((timeNow % day) % hour) / minute ;         //and so on...
+int seconds = (((timeNow % day) % hour) % minute) / second;
+
+// put your main code here, to run repeatedly: 
+Serial.print("          Time: H:");
+Serial.print(hours);
+Serial.print(" M:");
+Serial.print(minutes);
+Serial.print(" S:");
+Serial.print(seconds);
+Serial.print(" Millis:");
+
+Serial.println(millis());
 
 
 // delay(700);
- 
-  
-  
+
+
+
 }
 */
 
